b007c5805af2d2d7c144b5046173ecae10137e67
CordeiroOvelha <cordeiro.juttel@gmail.com>
2026-03-01 23:17:03 -0300

    Implementaçao do rule Switch no arquivo QuatroEstaçoes.java

--- Exercicios/QuatroEstaçoes.java
+++ Exercicios/QuatroEstaçoes.java
@@ -5,26 +5,25 @@ import javax.swing.JOptionPane;
 public class QuatroEstaçoes {
     public static void main(String[] args) {
     
-    String mouth;
-    
-    mouth = JOptionPane.showInputDialog(null, "Qual o mes voce quer buscar?");
-    
-    switch (mouth) {
-        case "janeiro", "fevereiro", "dezembro" -> 
-            JOptionPane.showMessageDialog(null, "Verão");
-            
-        case "março", "abril", "maio" -> 
-            JOptionPane.showMessageDialog(null, "outono");
-            
-        case "junho", "julho", "agosto" -> 
-            JOptionPane.showMessageDialog(null, "inverno");
-            
-        case "setembro", "outubro", "novembro" -> 
-            JOptionPane.showMessageDialog(null, "primavera");
-            
-        default -> 
-            JOptionPane.showMessageDialog(null, "escrito errado ou mes invalido");
-    }
-}	
-	}
+		String mouth;
+		
+		mouth = JOptionPane.showInputDialog(null, "Qual o mes voce quer buscar?");
+		
+		switch (mouth) {
+			case "janeiro", "fevereiro", "dezembro" -> 
+				JOptionPane.showMessageDialog(null, "Verão");
+				
+			case "março", "abril", "maio" -> 
+				JOptionPane.showMessageDialog(null, "outono");
+				
+			case "junho", "julho", "agosto" -> 
+				JOptionPane.showMessageDialog(null, "inverno");
+				
+			case "setembro", "outubro", "novembro" -> 
+				JOptionPane.showMessageDialog(null, "primavera");
+				
+			default -> 
+				JOptionPane.showMessageDialog(null, "escrito errado ou mes invalido");
+		}
+	}	
 }
