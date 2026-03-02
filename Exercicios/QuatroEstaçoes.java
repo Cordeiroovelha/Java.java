fc3e54c8b97984ac35b00df6540c2d78793ddc6b
CordeiroOvelha <cordeiro.juttel@gmail.com>
2026-03-01 23:15:04 -0300

    Emplementaçao do rule switch no arquivo QuatroEstaçoes.java

--- Exercicios/QuatroEstaçoes.java
+++ Exercicios/QuatroEstaçoes.java
@@ -4,65 +4,27 @@ import javax.swing.JOptionPane;
 
 public class QuatroEstaçoes {
     public static void main(String[] args) {
-		
-		String mouth;
-		
-		mouth = JOptionPane.showInputDialog(null, "Qual o mes voce quer buscar?");
-		
-		switch (mouth) {
-		case "janeiro": {
-			JOptionPane.showMessageDialog(null, "Verão");
-			break;
-		}
-		case "fevereiro": {
-			JOptionPane.showMessageDialog(null, "Verão");
-			break;
-		}
-		case "março": {
-			JOptionPane.showMessageDialog(null, "outono");
-			break;
-		}
-		case "abril": {
-			JOptionPane.showMessageDialog(null, "outono");
-			break;
-		}
-		case "maio": {
-			JOptionPane.showMessageDialog(null, "outono");
-			break;
-		}
-		case "junho": {
-			JOptionPane.showMessageDialog(null, "inverno");
-			break;
-		}
-		case "julho": {
-			JOptionPane.showMessageDialog(null, "inverno");
-			break;
-		}
-		case "agosto": {
-			JOptionPane.showMessageDialog(null, "inverno");
-			break;
-		}
-		case "setembro": {
-			JOptionPane.showMessageDialog(null, "primavera");
-			break;
-		}
-		case "outubro": {
-			JOptionPane.showMessageDialog(null, "primavera");
-			break;
-		}
-		case "novembro": {
-			JOptionPane.showMessageDialog(null, "primavera");
-			break;
-		}
-		case "dezembro": {
-			JOptionPane.showMessageDialog(null, "Verão");
-			break;
-		}
-		default:
-			JOptionPane.showMessageDialog(null, "escrito errado ou mes invalido");
-			break;
-		}
-		
-		
+    
+    String mouth;
+    
+    mouth = JOptionPane.showInputDialog(null, "Qual o mes voce quer buscar?");
+    
+    switch (mouth) {
+        case "janeiro", "fevereiro", "dezembro" -> 
+            JOptionPane.showMessageDialog(null, "Verão");
+            
+        case "março", "abril", "maio" -> 
+            JOptionPane.showMessageDialog(null, "outono");
+            
+        case "junho", "julho", "agosto" -> 
+            JOptionPane.showMessageDialog(null, "inverno");
+            
+        case "setembro", "outubro", "novembro" -> 
+            JOptionPane.showMessageDialog(null, "primavera");
+            
+        default -> 
+            JOptionPane.showMessageDialog(null, "escrito errado ou mes invalido");
+    }
+}	
 	}
 }
