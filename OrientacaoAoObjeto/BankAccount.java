2055a9a190bcf516223e74fd26d06502c22b07ae
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-04 20:29:45 -0300

    Add default constructor to BankAccount class
    Added a default constructor to the BankAccount class.

--- OrientacaoAoObjeto/BankAccount.java
+++ OrientacaoAoObjeto/BankAccount.java
@@ -1,36 +1,40 @@
 package OrientacaoAoObjeto;
 
 public class BankAccount {
-    private int id;
-	private String clientName;
-	private double balance;
-	
-	public BankAccount(int id, String clientName, double balance) {
-		this.id = id;
-		this.clientName = clientName;
-		this.balance = balance;
-	}
-	
-	public void withDraw(double value) {
-		if(value >= balance) { 
-			balance -= value;
+   private int id;
+		private String clientName;
+		private double balance;
+		
+		// construtor iniciado
+		public BankAccount(int id, String clientName, double balance) {
+			this.id = id;
+			this.clientName = clientName;
+			this.balance = balance;
 		}
-	}
-	
-	public void deposit(double value) {
-		if (value > 0) {
-			balance += value;
-		}
-	}
-	
-	public void displayDetails() {
-		System.out.println("Dados da conta: ");
-		System.out.println("--------------------------");
-		System.out.println("Codigo .: " + id);
-		System.out.println("Nome ...: " + clientName);
-		System.out.println("Saldo ..: R$ " + balance);
-		System.out.println("--------------------------");
 
-	}
+		// Construtro padrão
+		public BankAccount() {}
+		
+		public void withDraw(double value) {
+			if(value >= balance) { 
+				balance -= value;
+			}
+		}
+		
+		public void deposit(double value) {
+			if (value > 0) {
+				balance += value;
+			}
+		}
+		
+		public void displayDetails() {
+			System.out.println("Dados da conta: ");
+			System.out.println("--------------------------");
+			System.out.println("Codigo .: " + id);
+			System.out.println("Nome ...: " + clientName);
+			System.out.println("Saldo ..: R$ " + balance);
+			System.out.println("--------------------------");
+		}
 }
+
 
