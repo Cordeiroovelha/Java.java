9d03336e820adb0b472d58e6a8ab69bde2048968
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-11 19:03:20 -0300

    Update PeopleControlMain.java

--- OrientacaoAoObjeto/PeopleControlMain.java
+++ OrientacaoAoObjeto/PeopleControlMain.java
@@ -3,10 +3,10 @@ package OrientacaoAoObjeto;
 import java.util.Scanner;
 
 public class PeopleControlMain {
-    
-    public static int menu(){
-        private static Scanner keybord = new Scanner(System.in);
 
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static int menu(){
         System.out.println("---------------------------");
         System.out.println("-- People Manager System --");
         System.out.println("---------------------------");
@@ -16,14 +16,41 @@ public class PeopleControlMain {
         System.out.println("3 - Refresh");
         System.out.println("4 - Remove");
         System.out.println("5 - Exist ");
-        return keybord.nextInt();
+        return scanner.nextInt();
+    }
+
+    public static Person readData(){
+        System.out.println("Type the name: ");
+        String name = scanner.nextLine();
+        System.out.println("Type the Id: ");
+        String Id = scanner.nextLine();
+        return new Person(name, Id);
     }
 
     public static void main(String[] args) {
+        PeopleManager manager = new PeopleManager();
+        Person person;
         int option;
-
+        
         do {
             option = menu();
+            scanner.nextLine();
+
+            switch (option) {
+                case 1: {
+                    person = readData();
+                    if(manager.save(person)){
+                        System.out.println("Sign In complete");
+                        System.out.println(manager.getPeopleList());
+                    }
+                    else
+                        System.out.println("Error in Sign In");
+                    break;
+                }
+                default:
+                    break;
+            }
+
         } while (option != 5);
     }
 }
