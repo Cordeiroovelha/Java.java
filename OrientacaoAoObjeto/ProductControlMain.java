9e11da9a92abb16da06dc4dfbefbae16f59353fe
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-11 22:21:42 -0300

    Update ProductControlMain.java

--- OrientacaoAoObjeto/ProductControlMain.java
+++ OrientacaoAoObjeto/ProductControlMain.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class ProductControlMain {
     private static Scanner scanner = new Scanner(System.in);
+    private static ProductManager manager = new ProductManager();
 
-    public static int menu() {
+    private static int menu() {
         System.out.println("------------------------------");
         System.out.println("------------------------------");
         System.out.println("---- Controle de produtos ----");
@@ -16,22 +17,68 @@ public class ProductControlMain {
         System.out.println("1- Registro de novo produto");
         System.out.println("2- modificar dados de produto");
         System.out.println("------------------------------");
-        System.out.println("Selecione uma opçao: ");
+        System.out.print("Selecione uma opçao: ");
         return scanner.nextInt();
+    }
+
+    private static void register() {
+        System.out.println("Digite o nome: ");
+        String name = scanner.nextLine();
+        System.out.println("Digite Id: ");
+        String id = scanner.nextLine();
+        System.out.println("Digite o valor: ");
+        double value = scanner.nextDouble();
+        Product product = new Product(id, name, value);
+        if(manager.saveProduct(product)){
+            System.out.println("Produto registrado");
+            System.out.println(manager.getProductList());
+        }
+        else
+            System.out.println("Erro em registrar produto");
+    }
+
+    // colocar if de verificaçao duplicado fora do primeiro if
+    private static void modify() {
+        
+        System.out.println("O que quer mudar? \n[1] - nome  \n[2] - valor");
+        int opt = scanner.nextInt();
+        System.out.println("Qual o Id:");
+        String id = scanner.nextLine();
+        if(opt == 1){
+            System.out.println("Qual o novo nome: ");
+            String newName = scanner.nextLine();
+
+            if(manager.updateName(id, newName)){
+                System.out.println("Data update");
+            }else
+                System.out.println("Error in update");
+
+        }else{
+            System.out.println("Qual o novo valor: ");
+            double newValue = scanner.nextDouble();
+            if(manager.updateValue(id, newValue)){
+                System.out.println("Data update");
+            }else
+                System.out.println("Error in update");
+        }
     }
 
     public static void main(String[] args) {
         int option;
         option = menu();
+        scanner.nextLine();
+
         switch (option) {
             case 1:
-                
+                register();
                 break;
-        
+            case 2:
+                modify();
+                break;
             default:
                 break;
         }
     }
 
-    
+
 }
