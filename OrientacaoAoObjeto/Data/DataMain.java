adce85108a7995e0ef2bf9d96fe1ce6bf5ba43ab
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2026-03-17 20:41:37 -0300

    Add user input for date management

--- OrientacaoAoObjeto/Data/DataMain.java
+++ OrientacaoAoObjeto/Data/DataMain.java
@@ -1,13 +1,19 @@
 package OrientacaoAoObjeto.Data;
+import javax.swing.JOptionPane;
 
 public class DataMain {
-    
+    private static Data manager = new Data();
 
     public static void main(String[] args) {
         int day;
         int month;
         int year;
 
+        day = Integer.parseInt(JOptionPane.showInputDialog("Digite um dia: "));
+        month = Integer.parseInt(JOptionPane.showInputDialog("Digite um mes: "));
+        year = Integer.parseInt(JOptionPane.showInputDialog("Digite um ano: "));
+        manager.changeDate(day, month, year);
+        manager.printDate();
         
     }
 }
